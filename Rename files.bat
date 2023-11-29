55c6a5fa3890bd2ffcb9b335b1c1ac1e6202eebc
yxymeng <70695929+yxymeng@users.noreply.github.com>
2023-11-29 12:57:18 +0800

    Create Rename files.bat

---
+++ Rename files.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo 请输入你需要进行操作的序号
+echo 1.重命名所有文件
+echo 2.撤销重命名
+set /p answer="请输入1或2 ："
+if /i "%answer%"=="1" (
+    Rename Files.py
+) else if /i "%answer%"=="2" (
+    Undo Renamed.py
+)
+
+pause
