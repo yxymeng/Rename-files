fe850a201fcdd5257732664165b599429a67b749
yxymeng <70695929+yxymeng@users.noreply.github.com>
2023-11-29 16:52:57 +0800

    Update and rename Rename Files.bat to Rename_Files.bat

--- Rename_Files.bat
+++ Rename_Files.bat
@@ -5,9 +5,9 @@ echo 1.重命名所有文件
 echo 2.撤销重命名
 set /p answer="请输入1或2 ："
 if /i "%answer%"=="1" (
-    Rename Files.py
+    Rename_Files.py
 ) else if /i "%answer%"=="2" (
-    Undo Renamed.py
+    Undo_Renamed.py
 )
 
 pause
